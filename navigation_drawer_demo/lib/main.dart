28c57e5b6941578841fd19ea2597e0bcf3a820a0
JyotiAvhad <avhadjyoti09@gmail.com>
2020-12-11 18:35:07 +0530

    on item click open its pages in navigation drawer demo

--- navigation_drawer_demo/lib/main.dart
+++ navigation_drawer_demo/lib/main.dart
@@ -30,6 +30,8 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Text('Its Navigation Drawer Demo'),
       ),
+
+
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -44,46 +46,120 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
 
-            UserAccountsDrawerHeader(
-              accountName: Text("User"),
-              accountEmail: Text("user@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor:
-                    Theme.of(context).platform == TargetPlatform.android
-                        ? Colors.blue
-                        : Colors.white,
-                child: Text(
-                  "User",
-                  style: TextStyle(fontSize: 50.0),
-                ),
-              ),
-            ),
+
+            // UserAccountsDrawerHeader(
+            //   accountName: Text("User"),
+            //   accountEmail: Text("user@gmail.com"),
+            //   currentAccountPicture: CircleAvatar(
+            //     backgroundColor:
+            //         Theme.of(context).platform == TargetPlatform.android
+            //             ? Colors.blue
+            //             : Colors.white,
+            //     child: Text(
+            //       "User",
+            //       style: TextStyle(fontSize: 50.0),
+            //     ),
+            //   ),
+            // ),
 
 
             ListTile(
-              title: Text(
-                'Home',
-                style: TextStyle(color: Colors.pinkAccent),
-              ),
+              title: Text('Home',style: TextStyle(color: Colors.pinkAccent),),
               onTap: () {
-                Navigator.pop(context);
+                // Navigator.pop(context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
               },
             ),
+
+
             ListTile(
               title: Text('About US'),
               onTap: () {
-                Navigator.pop(context);
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutUsPage()),
+                );
               },
             ),
+
+
             ListTile(
               title: Text('Logout'),
               onTap: () {
-                Navigator.pop(context);
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LogoutPage()),
+                );
               },
             ),
+
           ],
         ),
       ),
     );
   }
 }
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('This is Home Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutUsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About Us"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('This is About Us Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class LogoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Logout"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('This is Logout Page'),
+        ),
+      ),
+    );
+  }
+}
+
